fd262e9ec9bdf0601f79fa5dec2139f9a9f75b6a
Darkestry <73218288+Darkestry@users.noreply.github.com>
2020-10-30 12:42:13 +0100

    Das Programm ist fertig und kommentiert 2.

--- DieRömer.py
+++ DieRömer.py
@@ -1,3 +1,4 @@
+
 '''
 Aufgabe: Schreiben Sie ein Python-Programm, das eine beliebige römische Zahl in eine
          „gewöhnliche” Dezimalzahl umrechnet.
@@ -5,9 +6,7 @@ Aufgabe: Schreiben Sie ein Python-Programm, das eine beliebige römische Zahl in
 
 roemische_zahlen = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000} # Erstelle ein Dictionary, "übersetze" römische Zahlen in Dezimalzahlen
 dezimal_zahlen = 0                                                                   # Instanziiere eine Variable mit dem Namen "dezimal_zahlen"
-
-if __name__ == "__main__":
-    input_roemisch = input("Römische Zahl eingeben: ")                               # User Input wird in input_roemisch "gespeichert"
+input_roemisch = input("Römische Zahl eingeben: ")                                   # User Input wird in input_roemisch "gespeichert"
 
 vorherige_zahl = 0                                                                   # Instanziiere eine Variable mit dem Namen "vorherige_zahl"
 for char in input_roemisch:                                                          # In Python ist es möglich, über einen String zu iterieren... Iteriere mit einer for-Schleife
@@ -16,6 +15,8 @@ for char in input_roemisch:                                                     
     else:                                                                            # Wenn die nachfolgende Ziffer/Zahl kleiner oder gleich groß ist wie die vorherige Ziffer/Zahl...
         dezimal_zahlen += vorherige_zahl                                             # Addiere die vorherige Zahl mit der nachfolgenden Zahl
     vorherige_zahl = roemische_zahlen[char]                                          # Übersetze die vorherige römische Zahl in eine Dezimalzahl
-dezimal_zahlen += vorherige_zahl                                                     # Addiere die vorherige Zahl zur Dezimalzahl
+    #print(roemische_zahlen[char])
+    #print(dezimal_zahlen)
+dezimal_zahlen += vorherige_zahl                                                     # Sobald die For-Schleife beendet wurde(wenn kein character mehr im Input zu finden ist), wird die letzte Ziffer zur Dezimalsumme addiert
 
 print("Als Dezimalzahl: " + str(dezimal_zahlen))                                     # Ausgabe der Dezimalzahl
